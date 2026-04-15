722d5a9415cbb51aad504898d037b78e09067c9d
Diego Caldas <juandiegoarmandoc@gmail.com>
2026-04-14 19:39:51 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/ERPMuni/ERPmuni/ErPmuniApplication.java
@@ -0,0 +1,13 @@
+package com.ERPMuni.ERPmuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErPmuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErPmuniApplication.class, args);
+	}
+
+}
