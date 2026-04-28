b8041c1c68301f03b9dd36736b98c81f1f6ce2c1
josssruizz <anarchiveofjo@gmail.com>
2026-04-27 20:09:19 -0500

    Frontend

---
+++ src/test/java/com/ERPMuni/ERPmuni/ErPmuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ERPMuni.ERPmuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErPmuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
