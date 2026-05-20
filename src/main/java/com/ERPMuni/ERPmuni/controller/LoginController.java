dedce43407cb5e758b114ce51af8a6ad21bb55da
Diego Caldas <juandiegoarmandoc@gmail.com>
2026-05-19 20:15:41 -0500

    feat: controlador del login
    controlador que maneja la vista del login

---
+++ src/main/java/com/ERPMuni/ERPmuni/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.ERPMuni.ERPmuni.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+    @GetMapping
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping
+    public String loginPost() {
+        return "login";
+    }
+}
